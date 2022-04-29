8a97ff7b44fc8674184a0e0ed9f22e0a8376e1a2
Ivanov Alexander <ivanovaa1992@gmail.com>
2022-04-29 14:57:14 +0300

    setup eslint

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import { cli } from "../src/cli.js";
+import cli from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 
-cli()
+cli();
--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,9 @@
 import readlineSync from 'readline-sync';
 
-export const cli = () => {
-    const name = readlineSync.question('May I have your name? ');
+const cli = () => {
+  const name = readlineSync.question('May I have your name? ');
 
-    console.log(`Hello, ${name}`)
-}
+  console.log(`Hello, ${name}`);
+};
 
+export default cli;
